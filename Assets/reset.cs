89aa175c7eaaea6e4190beaf4ea820b3ddd32744
unknown <jd794@cornell.edu>
2018-09-20 19:48:11 -0400

    restart

---
+++ Assets/reset.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class reset : MonoBehaviour {
+
+   void Update ()
+   {
+     if( Input.GetKeyDown(KeyCode.R) )
+     {
+       SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+     }
+   }
+ 
+}
