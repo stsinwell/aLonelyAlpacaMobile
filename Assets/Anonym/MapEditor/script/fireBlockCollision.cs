a719e14b531946f35353e8c35f3482ae83313a86
Yuji Yang <yy348@cornell.edu>
2018-09-23 21:23:30 -0400

    fire block collision

---
+++ Assets/Anonym/MapEditor/script/fireBlockCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class fireBlockCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        
+        if (hit.collider.tag == "FireBlock")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
