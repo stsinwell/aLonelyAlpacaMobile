e2106b51a68d379cbeb01976fce32be6b79defc4
Yuji Yang <yy348@cornell.edu>
2018-10-02 18:53:23 -0400

    script that handles changing alpaca directions

---
+++ Assets/Scripts/changeFacingDirection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeFacingDirection : MonoBehaviour {
+
+    public Sprite alpacaNE, alpacaNW, alpacaSE, alpacaSW;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.GetComponent<SpriteRenderer>().sprite = alpacaNE;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.GetComponent<SpriteRenderer>().sprite = alpacaSW;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.GetComponent<SpriteRenderer>().sprite = alpacaNW;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.GetComponent<SpriteRenderer>().sprite = alpacaSE;
+        }
+    }
+}
