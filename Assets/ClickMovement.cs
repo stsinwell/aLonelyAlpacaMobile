5e38f2cf9f0d960487f135c4b35b380d5af9c4bd
tedbauer <tjb272@cornell.edu>
2018-10-02 13:51:11 -0400

    Click to move

---
+++ Assets/ClickMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickMovement : MonoBehaviour
+{
+    //Make sure to attach these Buttons in the Inspector
+    public Button m_YourFirstButton, m_YourSecondButton;
+
+    void Start()
+    {
+        Button btn1 = m_YourFirstButton.GetComponent<Button>();
+        Button btn2 = m_YourSecondButton.GetComponent<Button>();
+
+        //Calls the TaskOnClick/TaskWithParameters method when you click the Button
+        btn1.onClick.AddListener(TaskOnClick);
+        btn2.onClick.AddListener(delegate {TaskWithParameters("Hello"); });
+    }
+
+    void TaskOnClick()
+    {
+        //Output this to console when the Button is clicked
+        Debug.Log("You have clicked the button!");
+    }
+
+    void TaskWithParameters(string message)
+    {
+        //Output this to console when the Button is clicked
+        Debug.Log(message);
+    }
+}
