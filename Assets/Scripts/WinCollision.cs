d4351dde440b47c08552a10215dec3ede1854056
unknown <jd794@cornell.edu>
2018-10-05 11:02:01 -0400

    fix merge

--- Assets/Scripts/WinCollision.cs
+++ Assets/Scripts/WinCollision.cs
@@ -5,12 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class WinCollision : MonoBehaviour {
 
-<<<<<<< HEAD
 	public string nextLevel;
 
-=======
-    public Object nextLevel;
->>>>>>> 40d7e43b381422a907d3d426e9f9323ab2a66441
 	// Use this for initialization
 	void Start () {
 		
@@ -22,11 +18,7 @@ public class WinCollision : MonoBehaviour {
 
 	void OnControllerColliderHit(ControllerColliderHit hit){
 		if(hit.collider.tag == "Goal"){
-<<<<<<< HEAD
 			SceneManager.LoadScene(nextLevel, LoadSceneMode.Single);
-=======
-			SceneManager.LoadScene(nextLevel.name, LoadSceneMode.Single);
->>>>>>> 40d7e43b381422a907d3d426e9f9323ab2a66441
 		}
 	}
 
