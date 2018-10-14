327f974da2f74924edc5f3f130bb755938ecbc08
unknown <jd794@cornell.edu>
2018-10-13 22:36:59 -0400

    anims

--- Assets/Scripts/changeFacingDirection.cs
+++ Assets/Scripts/changeFacingDirection.cs
@@ -7,7 +7,10 @@ public class changeFacingDirection : MonoBehaviour {
 
     public KeyInputAssist k;
 
+    public Animator animator;
+
     public Sprite alpacaNE, alpacaNW, alpacaSE, alpacaSW;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -17,7 +20,6 @@ public class changeFacingDirection : MonoBehaviour {
 	void Update () {
         if (k.lastFacing == KeyInputAssist.Facing.PosZ) 
         {
-            this.GetComponent<SpriteRenderer>().sprite = alpacaNE;
         }
         if (k.lastFacing == KeyInputAssist.Facing.NegZ)
         {
@@ -29,7 +31,16 @@ public class changeFacingDirection : MonoBehaviour {
         }
         if (k.lastFacing == KeyInputAssist.Facing.PosX)
         {
-            this.GetComponent<SpriteRenderer>().sprite = alpacaSE;
+            print("walking down");
+            //this.GetComponent<SpriteRenderer>().sprite = alpacaNE;
+            if(Input.GetKeyDown(KeyCode.D)){
+                animator.SetBool("walkse", true);
+                animator.SetInteger("countdown", 20);
+            }
+            else if(animator.GetInteger("countdown")>0){
+                animator.SetBool("walkse", false);
+                animator.SetInteger("countdown",(animator.GetInteger("countdown")-1));
+            }
         }
     }
 }
