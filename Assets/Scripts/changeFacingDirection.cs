c8146abb57cea3037c2c99abf8153ed5f03da947
unknown <jd794@cornell.edu>
2018-10-13 23:50:12 -0400

    anims?

--- Assets/Scripts/changeFacingDirection.cs
+++ Assets/Scripts/changeFacingDirection.cs
@@ -18,28 +18,56 @@ public class changeFacingDirection : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (k.lastFacing == KeyInputAssist.Facing.PosZ) 
-        {
+        if (k.lastFacing == KeyInputAssist.Facing.PosZ) {
+            //this.GetComponent<SpriteRenderer>().sprite = alpacaNE;
+            if(Input.GetKeyDown(KeyCode.W) && !animator.GetBool("walkne")){
+                animator.SetBool("walkne", true);
+                animator.SetInteger("countdown", 20);
+            }
+            else if(animator.GetInteger("countdown")>0 && animator.GetBool("walkne")){
+                animator.SetInteger("countdown",(animator.GetInteger("countdown")-1));
+            }
+            else if(animator.GetInteger("countdown")==0 && animator.GetBool("walkne")){
+                animator.SetBool("walkne", false);
+            }
         }
         if (k.lastFacing == KeyInputAssist.Facing.NegZ)
         {
-            this.GetComponent<SpriteRenderer>().sprite = alpacaSW;
+            if(Input.GetKeyDown(KeyCode.S) && !animator.GetBool("walksw")){
+                animator.SetBool("walksw", true);
+                animator.SetInteger("countdown", 20);
+            }
+            else if(animator.GetInteger("countdown")>0 && animator.GetBool("walksw")){
+                animator.SetInteger("countdown",(animator.GetInteger("countdown")-1));
+            }
+            else if(animator.GetInteger("countdown")==0 && animator.GetBool("walksw")){
+                animator.SetBool("walksw", false);
+            }
         }
-        if (k.lastFacing == KeyInputAssist.Facing.NegX)
-        {
-            this.GetComponent<SpriteRenderer>().sprite = alpacaNW;
+        if (k.lastFacing == KeyInputAssist.Facing.NegX){
+            //this.GetComponent<SpriteRenderer>().sprite = alpacaNW;
+            if(Input.GetKeyDown(KeyCode.A) && !animator.GetBool("walknw")){
+                animator.SetBool("walknw", true);
+                animator.SetInteger("countdown", 20);
+            }
+            else if(animator.GetInteger("countdown")>0 && animator.GetBool("walknw")){
+                animator.SetInteger("countdown",(animator.GetInteger("countdown")-1));
+            }
+            else if(animator.GetInteger("countdown")==0 && animator.GetBool("walknw")){
+                animator.SetBool("walknw", false);
+            }
         }
         if (k.lastFacing == KeyInputAssist.Facing.PosX)
         {
-            print("walking down");
-            //this.GetComponent<SpriteRenderer>().sprite = alpacaNE;
-            if(Input.GetKeyDown(KeyCode.D)){
+            if(Input.GetKeyDown(KeyCode.D) && !animator.GetBool("walkse")){
                 animator.SetBool("walkse", true);
                 animator.SetInteger("countdown", 20);
             }
-            else if(animator.GetInteger("countdown")>0){
-                animator.SetBool("walkse", false);
+            else if(animator.GetInteger("countdown")>0 && animator.GetBool("walkse")){
                 animator.SetInteger("countdown",(animator.GetInteger("countdown")-1));
+            }
+            else if(animator.GetInteger("countdown")==0 && animator.GetBool("walkse")){
+                animator.SetBool("walkse", false);
             }
         }
     }
