e37395c04a5b8df7ade843feb4edea9ebbe95be7
tedbauer <tjb272@cornell.edu>
2018-10-24 23:10:50 -0400

    Add back reset lol

---
+++ Assets/Anonym/MapEditor/script/Reset.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Reset : MonoBehaviour {
+
+	public Image deathImage;
+
+	// Use this for initialization
+	void Start () {
+
+		deathImage.enabled = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+        if (Input.GetKeyDown(KeyCode.R)) {
+			Restart();
+        }
+		
+	}
+
+	public void Restart() {
+		LoggingManager.instance.RecordEvent(1, "Player pressed Restart button.");
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
