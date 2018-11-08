aa3132fe7f0556b69caa4c6c83c7b7ae17c62f5f
tedbauer <tjb272@cornell.edu>
2018-11-08 12:07:02 -0500

    Sticky blocks

--- Assets/Anonym/MapEditor/script/clickable_block.cs
+++ Assets/Anonym/MapEditor/script/clickable_block.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class clickable_block : MonoBehaviour {
     public bool isSelected;
     public bool isPlayerFacing;
+
+    public bool isSticky;
     Color highlightedColor;
 	SpriteRenderer sr;
 
 	// initializer
 	void Start () {
         isSelected = false;
+        isSticky = false;
         isPlayerFacing = false;
         highlightedColor = new Color(0.835f, 0.878f, 1.0f, 1.0f);
 		sr = GetComponentInChildren<SpriteRenderer>();
