b06a94a9ca644253728dd2216f97ab08dfba8843
Yuji Yang <yy348@cornell.edu>
2018-11-08 21:48:03 -0500

    Level Select Menu almost done! Just need to add new banners

--- Assets/ChangeLevelBanner.cs
+++ Assets/ChangeLevelBanner.cs
@@ -8,10 +8,13 @@ using UnityEngine.EventSystems;
 public class ChangeLevelBanner : MonoBehaviour, IPointerEnterHandler {
 	public Image levelBanner;
 	int defaultLevelNumber;
+	int levelPassed;
 
 	// Use this for initialization
 	void Start () {
-		defaultLevelNumber = GameObject.Find("LevelMenuController").GetComponent<LevelMenuControllerScript>().levelPassed + 1;
+		levelPassed = GameObject.Find("LevelMenuController").GetComponent<LevelMenuControllerScript>().levelPassed;
+		if(levelPassed < 22) defaultLevelNumber = levelPassed + 1;
+		if(levelPassed == 22) defaultLevelNumber = levelPassed;
 		levelBanner = GameObject.Find("LevelBanner").GetComponent<Image>();
 		levelBanner.sprite = GameObject.Find("banner" + defaultLevelNumber).GetComponent<SpriteRenderer>().sprite;
 	}
@@ -23,8 +26,7 @@ public class ChangeLevelBanner : MonoBehaviour, IPointerEnterHandler {
 
 	public void OnPointerEnter(PointerEventData eventData)
      {
-         string getLevelNumber = Regex.Match(gameObject.name, @"\d+").Value;
-		Debug.Log("get level number is " + getLevelNumber);
+        string getLevelNumber = Regex.Match(gameObject.name, @"\d+").Value;
 		levelBanner.sprite = GameObject.Find("banner" + getLevelNumber).GetComponent<SpriteRenderer>().sprite;
      }
 }
--- Assets/LevelMenuControllerScript.cs
+++ Assets/LevelMenuControllerScript.cs
@@ -10,18 +10,16 @@ public class LevelMenuControllerScript : MonoBehaviour {
 
 	public Button B1Button, B2Button, B3Button, B4Button, B5Button, B6Button, 
 				  B7Button, B8Button, B9Button, B10Button, B11Button, B12Button,
-				  B13Button, B14Button, B15Button;
+				  B13Button, B14Button, B15Button, B16Button, B17Button, B18Button, 
+				  B19Button, B20Button, B21Button, B22Button;
 	public int levelPassed;
 
 	public Sprite alpacaLeft;
 	public Sprite alpacaRight;
 
-	//public Image levelBanner;
-
 	// Use this for initialization
 	void Start () {
 		levelPassed = PlayerPrefs.GetInt("LevelPassed");
-		//levelBanner = GameObject.Find("LevelBanner").GetComponent<Image>();
 
 		B1Button.interactable = true;
 		B2Button.interactable = false;
@@ -38,6 +36,13 @@ public class LevelMenuControllerScript : MonoBehaviour {
 		B13Button.interactable = false;
 		B14Button.interactable = false;
 		B15Button.interactable = false;
+		B16Button.interactable = false;
+		B17Button.interactable = false;
+		B18Button.interactable = false;
+		B19Button.interactable = false;
+		B20Button.interactable = false;
+		B21Button.interactable = false;
+		B22Button.interactable = false;
 
 		switch(levelPassed){
 			case 1:
@@ -173,7 +178,172 @@ public class LevelMenuControllerScript : MonoBehaviour {
 				B14Button.interactable = true;
 				B15Button.interactable = true;
 				break;
+			case 15:
+				B2Button.interactable = true;
+				B3Button.interactable = true;
+				B4Button.interactable = true;
+				B5Button.interactable = true;
+				B6Button.interactable = true;
+				B7Button.interactable = true;
+				B8Button.interactable = true;
+				B9Button.interactable = true;
+				B10Button.interactable = true;
+				B11Button.interactable = true;
+				B12Button.interactable = true;
+				B13Button.interactable = true;
+				B14Button.interactable = true;
+				B15Button.interactable = true;
+				B16Button.interactable = true;
+				break;
+			case 16:
+				B2Button.interactable = true;
+				B3Button.interactable = true;
+				B4Button.interactable = true;
+				B5Button.interactable = true;
+				B6Button.interactable = true;
+				B7Button.interactable = true;
+				B8Button.interactable = true;
+				B9Button.interactable = true;
+				B10Button.interactable = true;
+				B11Button.interactable = true;
+				B12Button.interactable = true;
+				B13Button.interactable = true;
+				B14Button.interactable = true;
+				B15Button.interactable = true;
+				B16Button.interactable = true;
+				B17Button.interactable = true;
+				break;
+			case 17:
+				B2Button.interactable = true;
+				B3Button.interactable = true;
+				B4Button.interactable = true;
+				B5Button.interactable = true;
+				B6Button.interactable = true;
+				B7Button.interactable = true;
+				B8Button.interactable = true;
+				B9Button.interactable = true;
+				B10Button.interactable = true;
+				B11Button.interactable = true;
+				B12Button.interactable = true;
+				B13Button.interactable = true;
+				B14Button.interactable = true;
+				B15Button.interactable = true;
+				B16Button.interactable = true;
+				B17Button.interactable = true;
+				B18Button.interactable = true;
+				break;
+			case 18:
+				B2Button.interactable = true;
+				B3Button.interactable = true;
+				B4Button.interactable = true;
+				B5Button.interactable = true;
+				B6Button.interactable = true;
+				B7Button.interactable = true;
+				B8Button.interactable = true;
+				B9Button.interactable = true;
+				B10Button.interactable = true;
+				B11Button.interactable = true;
+				B12Button.interactable = true;
+				B13Button.interactable = true;
+				B14Button.interactable = true;
+				B15Button.interactable = true;
+				B16Button.interactable = true;
+				B17Button.interactable = true;
+				B18Button.interactable = true;
+				B19Button.interactable = true;
+				break;
+			case 19:
+				B2Button.interactable = true;
+				B3Button.interactable = true;
+				B4Button.interactable = true;
+				B5Button.interactable = true;
+				B6Button.interactable = true;
+				B7Button.interactable = true;
+				B8Button.interactable = true;
+				B9Button.interactable = true;
+				B10Button.interactable = true;
+				B11Button.interactable = true;
+				B12Button.interactable = true;
+				B13Button.interactable = true;
+				B14Button.interactable = true;
+				B15Button.interactable = true;
+				B16Button.interactable = true;
+				B17Button.interactable = true;
+				B18Button.interactable = true;
+				B19Button.interactable = true;
+				B20Button.interactable = true;
+				break;
+			case 20:
+				B2Button.interactable = true;
+				B3Button.interactable = true;
+				B4Button.interactable = true;
+				B5Button.interactable = true;
+				B6Button.interactable = true;
+				B7Button.interactable = true;
+				B8Button.interactable = true;
+				B9Button.interactable = true;
+				B10Button.interactable = true;
+				B11Button.interactable = true;
+				B12Button.interactable = true;
+				B13Button.interactable = true;
+				B14Button.interactable = true;
+				B15Button.interactable = true;
+				B16Button.interactable = true;
+				B17Button.interactable = true;
+				B18Button.interactable = true;
+				B19Button.interactable = true;
+				B20Button.interactable = true;
+				B21Button.interactable = true;
+				break;
+			case 21:
+				B2Button.interactable = true;
+				B3Button.interactable = true;
+				B4Button.interactable = true;
+				B5Button.interactable = true;
+				B6Button.interactable = true;
+				B7Button.interactable = true;
+				B8Button.interactable = true;
+				B9Button.interactable = true;
+				B10Button.interactable = true;
+				B11Button.interactable = true;
+				B12Button.interactable = true;
+				B13Button.interactable = true;
+				B14Button.interactable = true;
+				B15Button.interactable = true;
+				B16Button.interactable = true;
+				B17Button.interactable = true;
+				B18Button.interactable = true;
+				B19Button.interactable = true;
+				B20Button.interactable = true;
+				B21Button.interactable = true;
+				B22Button.interactable = true;
+				break;
+			case 22:
+				B2Button.interactable = true;
+				B3Button.interactable = true;
+				B4Button.interactable = true;
+				B5Button.interactable = true;
+				B6Button.interactable = true;
+				B7Button.interactable = true;
+				B8Button.interactable = true;
+				B9Button.interactable = true;
+				B10Button.interactable = true;
+				B11Button.interactable = true;
+				B12Button.interactable = true;
+				B13Button.interactable = true;
+				B14Button.interactable = true;
+				B15Button.interactable = true;
+				B16Button.interactable = true;
+				B17Button.interactable = true;
+				B18Button.interactable = true;
+				B19Button.interactable = true;
+				B20Button.interactable = true;
+				B21Button.interactable = true;
+				B22Button.interactable = true;
+				break;
 		}
+			
+			
 
 		positionAlpacaOnRecentLevel(levelPassed);
 	}
@@ -185,22 +355,24 @@ public class LevelMenuControllerScript : MonoBehaviour {
 	}
 
 	public void positionAlpacaOnRecentLevel(int mostRecentLevel) {
-		string findThisButton = "B" + (mostRecentLevel + 1).ToString() + "Button";
+		string findThisButton;
+
+		// Change the conditional values to match whatever the last level is
+		findThisButton = "B" + (mostRecentLevel + 1).ToString() + "Button";
+		if (mostRecentLevel == 22) findThisButton = "B" + (mostRecentLevel).ToString() + "Button";
+
 		GameObject mostRecentStar = GameObject.Find(findThisButton);
 		Vector3 mostRecentStarPosition = mostRecentStar.GetComponent<RectTransform>().position;
 
 		GameObject alpacaSprite = GameObject.Find("AlpacaSprite");
 		alpacaSprite.GetComponent<RectTransform>().position = mostRecentStarPosition + new Vector3(0, 40, 0);
-		if (mostRecentLevel >= 1 && mostRecentLevel <= 6) alpacaSprite.GetComponent<Image>().sprite = alpacaLeft;
-		if (mostRecentLevel >= 7 && mostRecentLevel <= 14) alpacaSprite.GetComponent<Image>().sprite = alpacaRight;
+		if ((mostRecentLevel >= 1 && mostRecentLevel <= 6)
+		|| (mostRecentLevel >= 14 && mostRecentLevel <= 18)
+		|| (mostRecentLevel == 22)) alpacaSprite.GetComponent<Image>().sprite = alpacaLeft;
+		if ((mostRecentLevel >= 7 && mostRecentLevel <= 13)
+		|| (mostRecentLevel >= 19 && mostRecentLevel <= 21)) alpacaSprite.GetComponent<Image>().sprite = alpacaRight;
 	
 	}
-
-	/* public void ChangeLevelBanner(BaseEventData BaseEventData) {
-		string getLevelNumber = Regex.Match(BaseEventData.selectedObject.name, @"\d+").Value;
-		Debug.Log("get level number is " + getLevelNumber);
-		levelBanner.sprite = GameObject.Find("banner" + getLevelNumber).GetComponent<SpriteRenderer>().sprite;
-	}*/
 	
 	// Update is called once per frame
 	void Update () {
