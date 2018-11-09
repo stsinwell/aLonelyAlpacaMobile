80f9ff7f89e24d2e2ad478c3ecc792bd8aab6cdb
unknown <jd794@cornell.edu>
2018-11-08 20:03:59 -0500

    intro

---
+++ Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CutsceneController : MonoBehaviour {
+
+	/** Object of next cut scene */
+	public GameObject nextScene;
+	/** Image of next cut scene */
+	private Image nextSceneImage;
+	/** Image of the current cut scene */
+	private Image curScene;
+	/** FadeImage Script for this Scene */
+	private FadeImage FIScript;
+	/** Amount of time the current scene should stay for before
+	    auto switching to the next */
+	public float currSceneTime;
+	/** Accumulator of time passed between frames */
+	private float timeAccum;
+	/** True if actively fading Out */
+	private bool fadeActive = false;
+
+	private bool wasEnabled = false;
+	private bool endScene = false;
+
+	private static int acc;
+	private int id;
+	// Use this for initialization
+	void Start () {
+		curScene = GetComponent<Image>();
+		if(nextScene != null)
+			nextSceneImage = nextScene.GetComponent<Image>();
+		FIScript = GetComponent<FadeImage>();
+		id = acc;
+		acc++;
+	}
+	// Update is called once per frame
+	void Update () {
+		if(curScene.enabled){
+			wasEnabled = true;
+			if(!fadeActive){
+				timeAccum += Time.deltaTime;
+				if(timeAccum >= currSceneTime){
+					//print("calling fade out " + id);
+					FIScript.FadeOut();
+					fadeActive = true;
+					nextSceneImage.enabled = true;
+				}
+			}
+		}
+		else if(!endScene && wasEnabled){
+			//print("disabled "+id);
+			endScene = true;
+			if(nextScene == null){
+				SceneManager.LoadScene("B1", LoadSceneMode.Single);
+			}
+			// else nextSceneImage.enabled = true;
+		}
+	}
+}
---
+++ Assets/Scripts/FadeImage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FadeImage : MonoBehaviour {
+
+	/** Rate to fade image **/	
+	private float fadeRate;
+	/** Image being faded **/
+	private Image image;
+	/** Alpha value being targeted **/
+	private float targetAlpha;
+	/** Alpha value approximating zero **/
+	private float alphaCutoff = 0.01f;
+	
+	private bool fadeOutEngaged;
+
+	// Use this for initialization
+	void Start (){
+	    image = GetComponent<Image>();
+		targetAlpha = image.color.a;
+		fadeRate = 5.0f;
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		Color curColor = image.color;
+		float alphaDiff = Mathf.Abs(curColor.a - targetAlpha);
+		if (alphaDiff>alphaCutoff){
+			curColor.a = Mathf.Lerp(curColor.a, targetAlpha, fadeRate*Time.deltaTime);
+			image.color = curColor;
+		}
+		else if(fadeOutEngaged){
+			image.enabled = false;
+		}
+	}
+
+	public void FadeOut(){
+		targetAlpha = 0.0f;
+		fadeOutEngaged = true;
+	}
+
+	public void FadeIn(){
+		targetAlpha = 1.0f;
+	}
+}
