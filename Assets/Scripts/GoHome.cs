97caa4a8b363ede9b8651afd9767b8386a58fcff
mlelien <emily.c.lien@gmail.com>
2019-02-13 16:58:45 -0500

    return button now goes home if level select is clicked from home

--- Assets/Scripts/GoHome.cs
+++ Assets/Scripts/GoHome.cs
@@ -3,38 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GoHome : MonoBehaviour {
+public class GoHome : MonoBehaviour
+{
 
-	public string menuLevel;
-	public string levelSelect;
-	GameObject currentLevel;
-	currentLevelName currentLevelScript;
-	GameObject previousLevel;
-	currentLevelName previousLevelScript;
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	public void goHome() {
-		SceneManager.LoadScene(menuLevel, LoadSceneMode.Single);
-		LoggingManager.instance.RecordEvent(7, "Player pressed HOME button.");
-	}
+  public string menuLevel;
+  public string levelSelect;
+  GameObject currentLevel;
+  currentLevelName currentLevelScript;
+  GameObject previousLevel;
+  currentLevelName previousLevelScript;
 
-	public void goToLevelSelect() {
-		currentLevel = GameObject.Find("GameObject");
-		currentLevelScript = currentLevel.GetComponent<currentLevelName>();
-		currentLevelScript.currentLevelNameString = SceneManager.GetActiveScene().name;
-		Debug.Log("current level name: " + currentLevelScript.currentLevelNameString);		
-	
-		SceneManager.LoadScene(levelSelect, LoadSceneMode.Single);
-		LoggingManager.instance.RecordEvent(7, "Player pressed LEVEL SELECT button.");
-	}
+  // Use this for initialization
+  void Start()
+  {
 
-	public void goBackToPreviousLevel(){
-		previousLevel = GameObject.Find("GameObject");
-		previousLevelScript = previousLevel.GetComponent<currentLevelName>();
-		SceneManager.LoadScene(previousLevelScript.currentLevelNameString, LoadSceneMode.Single);
-	}
+  }
+
+  public void goHome()
+  {
+    SceneManager.LoadScene(menuLevel, LoadSceneMode.Single);
+    LoggingManager.instance.RecordEvent(7, "Player pressed HOME button.");
+  }
+
+  public void goToLevelSelect()
+  {
+    currentLevel = GameObject.Find("GameObject");
+    currentLevelScript = currentLevel.GetComponent<currentLevelName>();
+    currentLevelScript.currentLevelNameString = SceneManager.GetActiveScene().name;
+    Debug.Log("current level name: " + currentLevelScript.currentLevelNameString);
+
+    SceneManager.LoadScene(levelSelect, LoadSceneMode.Single);
+    LoggingManager.instance.RecordEvent(7, "Player pressed LEVEL SELECT button.");
+  }
+
+  public void goBackToPreviousLevel()
+  {
+    previousLevel = GameObject.Find("GameObject");
+    if (previousLevel == null)
+    {
+      goHome();
+    }
+    else
+    {
+      previousLevelScript = previousLevel.GetComponent<currentLevelName>();
+      SceneManager.LoadScene(previousLevelScript.currentLevelNameString, LoadSceneMode.Single);
+    }
+  }
 }
