20f7ccb7ab9467a2a45eefb16317b96f260c4bf0
Susan Wenshan Zou <swz6@cornell.edu>
2019-06-18 20:36:38 -0700

    scaling tutorial arrows

--- Assets/Scripts/MVC/Controller/Lvl1Tutorial.cs
+++ Assets/Scripts/MVC/Controller/Lvl1Tutorial.cs
@@ -17,7 +17,11 @@ public class Lvl1Tutorial : MonoBehaviour
 	public Image tutImage;
 	// used to highlight four quadrants
 	public GameObject quadrant_0, quadrant_1, quadrant_2, quadrant_3;
+    public GameObject allarrows;
 	private Image[] quadrants;
+
+    // scaling used to keep tutorial images proper size
+    private Vector3 SCALE = new Vector3(Screen.width * 0.001f, Screen.width * 0.001f, 1);
 
 	bool didFade = false;
     // Start is called before the first frame update
@@ -31,6 +35,18 @@ public class Lvl1Tutorial : MonoBehaviour
 		quadrants[1].enabled = false;
 		quadrants[2].enabled = false;
 		quadrants[3].enabled = false;
+
+        Debug.Log(Screen.width);
+        if(quadrants[0] != null) 
+            quadrants[0].rectTransform.localScale = quadrants[1].rectTransform.localScale 
+                = quadrants[2].rectTransform.localScale = quadrants[3].rectTransform.localScale 
+                = SCALE;
+
+        if(tutImage != null)
+            tutImage.GetComponent<Image>().rectTransform.localScale = SCALE;
+
+        if(allarrows != null)
+            allarrows.GetComponent<Image>().rectTransform.localScale = SCALE;
 
         if(tutImage != null) {
             tutImage.color = new Color(1, 1, 1, 0);
