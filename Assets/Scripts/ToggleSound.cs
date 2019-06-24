12ebaa7e73f7107bbd5e61d5710d3b9f2d5b6b63
Susan Zou <swz6@cornell.edu>
2019-06-24 04:44:41 -0400

    sound

--- Assets/Scripts/ToggleSound.cs
+++ Assets/Scripts/ToggleSound.cs
@@ -1,26 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum musicLevel : byte {Off, Half, Full};
 
 public class ToggleSound : MonoBehaviour {
 
-	//private AudioListener audioListener;
-	static bool t = true;
+	static musicLevel level = musicLevel.Full;
+	public Sprite musicFull;
+	public Sprite musicHalf;
+	public Sprite musicOff;
+	private Image musicImage;
 
 	// Use this for initialization
 	void Start () {
+		musicImage = GetComponent<Image>();
+		musicImage.sprite = (level == musicLevel.Full) ? musicFull :
+								((level == musicLevel.Half) ? musicHalf : musicOff);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
 
-	public void toggle(){
-		t = !t;
-		if (t)
-			AudioListener.volume = 1f;
-		else
-			AudioListener.volume = 0f;
+	public void toggle () {
+		level = ((level == musicLevel.Full) ? musicLevel.Half : 
+					((level == musicLevel.Half) ? musicLevel.Off : musicLevel.Full));
+
+		switch (level) {
+			case musicLevel.Full:
+				musicImage.sprite = musicFull;
+				AudioListener.volume = 1f;
+				break;
+			case musicLevel.Half:
+				musicImage.sprite = musicHalf;
+				AudioListener.volume = 0.5f;
+				break;
+			case musicLevel.Off:
+				musicImage.sprite = musicOff;
+				AudioListener.volume = 0f;
+				break;
+		}
 	}
 
 }
